ffb6bf94cd53b681119d3036e93350f55ccc8aa4
Феофилов Максим <e1ektr0@live.ru>
2014-06-10 14:50:59 +0400

    Начало разработки парсера дерева

--- DebugConsole/Program.cs
+++ DebugConsole/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Reflection;
 using Antlr.Runtime;
 using Antlr.Runtime.Tree;
+using ApexParser.ApexNodes;
 
 namespace DebugConsole
 {
@@ -16,6 +17,14 @@ namespace DebugConsole
             var method = methodInfos;
             return (AstParserRuleReturnScope<CommonTree, IToken>)method.Invoke(parser, new object[0]);
         }
+        public static IApexNode JavaSource(this JavaTreeParser parser)
+        {
+            var type = parser.GetType();
+            //var x = parser.javaSource();
+            var methodInfos = type.GetMethod("javaSource", BindingFlags.NonPublic | BindingFlags.Instance);
+            var method = methodInfos;
+            return (IApexNode)method.Invoke(parser, new object[0]);
+        }
     }
 
     public class ApaexParser
@@ -26,10 +35,14 @@ namespace DebugConsole
             var parser = new JavaParser(tokens);
             parser.enableErrorMessageCollection(true);
             var tree = parser.JavaSource().Tree;
-            if (!parser.hasErrors())
+            var nodes = new CommonTreeNodeStream(tree);
+            var xs = new JavaTreeParser(nodes);
+            xs.enableErrorMessageCollection(true);
+            var rootNode = xs.JavaSource();
+            if (!parser.hasErrors() && !xs.hasErrors())
                 return;
 
-            foreach (var message in parser.getMessages())
+            foreach (var message in xs.getMessages())
             {
                 Console.WriteLine(message);
             }
